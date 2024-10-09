ea0785c9878d53c12493b644b7d36328a495986f
Dina-Ahmed <dina20906@fci.bu.edu.eg>
2024-10-10 02:05:07 +0300

    update index

--- src/modules/order/routers/order.router.js
+++ src/modules/order/routers/order.router.js
@@ -1,33 +1,34 @@
-
-
-import express from 'express'
-import { protectRoutes } from '../../auth/auth.controller.js'
-import { addOrder, createOnlinePayment, getAllOrders, getorder, getorderById, onlinePayment } from '../controllers/order.controller.js'
+import express from "express";
+import { protectRoutes } from "../../auth/auth.controller.js";
+import {
+  addOrder,
+  getAllOrders,
+  getorder,
+  getorderById,
+  onlinePayment,
+} from "../controllers/order.controller.js";
 const app = express();
 
+const OrderRouters = express.Router();
 
+OrderRouters.route("/:id")
+  .post(protectRoutes, addOrder)
+  .get(protectRoutes, getorderById);
 
-const OrderRouters=express.Router()
+OrderRouters.route("/")
+  .get(protectRoutes, getAllOrders)
+  .get(protectRoutes, getorder);
 
-OrderRouters.route('/:id')
-.post(protectRoutes,addOrder)
-.get(protectRoutes,getorderById)
-
-
-OrderRouters.route('/')
-.get(protectRoutes,getAllOrders)
-.get(protectRoutes,getorder)
-
-OrderRouters.route('/checkout/:id')
-.post(protectRoutes,onlinePayment)
+OrderRouters.route("/checkout/:id").post(protectRoutes, onlinePayment);
 // Add the webhook route with express.raw() middleware
-OrderRouters.post(
-    '/api/webhook',
-    express.raw({ type: 'application/json' }),  createOnlinePayment );
+// OrderRouters.post(
+//   "/api/webhook",
+//   express.raw({ type: "application/json" }),
+//   createOnlinePayment
+// );
 
 // OrderRouters.post(
 //     "/api/webhook",
 //     express.raw({ type: "application/json" }),createOnlinePayment)
 
-
-export default OrderRouters
+export default OrderRouters;
