90d58901685c294dc345e0c2d51be86d345bff8a
Dina-Ahmed <dina20906@fci.bu.edu.eg>
2024-10-27 12:49:19 +0300

    update

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const connecttodb=()=>{
     mongoose.connect(process.env.DB_CONNECTION)
-    .then(()=>{console.log("DB connected")})
+    .then(()=>{console.log("DB connected hello")})
     .catch(()=>{console.error('DB connection error')})
 }
 export default connecttodb
