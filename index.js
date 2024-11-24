bdc8f8b6b6324873c55b07e0101a134066e04130
Dina-DTS <dina20906@fci.bu.edu.eg>
2024-11-24 04:38:55 +0200

    delete comments

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.post(
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
-
-// Increase the limit for URL-encoded payloads
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/uploads", express.static("uploads"));
@@ -31,11 +29,7 @@ app.use("*", (req, res, next) => {
   next(new AppError("URL Not Found", 404));
 });
 
-//hello
 //global error to handle handleerror middleware
-// app.use((err, req, res, next) => {
-//   res.status(err.statusCode).json({ message: err.message, stack: err.stack });
-// });
 app.use((err, req, res, next) => {
   const isDevelopment = process.env.MODE === 'development';
   res.status(err.statusCode || 500).json({
